9683481d40991d26ee386e3bacdc1efb2a232f54
Thomas26948 <thomas.nigoghossian@ensta-paris.fr>
2023-11-28 23:59:06 +0100

    Fix double inference bug

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,26 @@ const imageInput = document.getElementById('imageInput');
 const processButton = document.getElementById('processButton');
 const uploadedImage = document.getElementById('uploadedImage');
 const fileInputLabel = document.querySelector('.custom-file-input-label');
+const imageContainer = document.getElementById('imageContainer');
+
+
+
+imageContainer.addEventListener('click', (event) => {
+  const isPasteClick = event.target === pasteBox;
+  if (isPasteClick) {
+    // Handle logic for pasting an image here
+    // Implement the necessary logic for pasting an image
+    // For example:
+    // const blob = ... // Get the image blob from the clipboard
+    // displayImage(blob);
+    // Process the pasted image
+
+pasteBox.click(); // Trigger the click event of the hidden file input
+  } else {
+
+  imageInput.click(); // Trigger the click event of the hidden file input
+  }
+});
 
 // Event listeners
 imageInput.addEventListener('change', handleImageInputChange);
@@ -93,56 +113,60 @@ async function processImage(imageFile) {
   const image = new Image();
   image.src = URL.createObjectURL(imageFile);
 
-  const session = await ort.InferenceSession.create('../model/mobilenet.onnx');
+  if (model_mobilenet == null) {
+    model_mobilenet = await ort.InferenceSession.create('../model/mobilenet.onnx');
+  }
+
 
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
   canvas.width = 224;
   canvas.height = 224;
-  ctx.drawImage(image, 0, 0, 224, 224);
-  const imageData = ctx.getImageData(0, 0, 224, 224).data;
+  await new Promise((resolve) => {
+    image.onload = () => {
+      ctx.drawImage(image, 0, 0, 224, 224);
+      const imageData = ctx.getImageData(0, 0, 224, 224).data;
 
-  // Convert image data to a Float32Array
-  // const inputArray = new Float32Array(imageData);
-  console.log(image.height);
-  console.log(image.width);
-  const inputArray = new Float32Array(3 * 224 * 224);
-  const inputTensor = new ort.Tensor('float32', inputArray, [1, 3, 224, 224]);
+      const inputArray = new Float32Array(3 * 224 * 224);
 
-  for (let i = 0; i < 224 * 224; i++) {
-    inputArray[i] = imageData[i * 4] / 255; // Red channel
-    inputArray[i + 224 * 224] = imageData[i * 4 + 1] / 255; // Green channel
-    inputArray[i + 2 * 224 * 224] = imageData[i * 4 + 2] / 255; // Blue channel
-  }
+      // Populate the inputArray with normalized pixel values
+      for (let i = 0; i < 224 * 224; i++) {
+        inputArray[i] = imageData[i * 4] / 255; // Red channel
+        inputArray[i + 224 * 224] = imageData[i * 4 + 1] / 255; // Green channel
+        inputArray[i + 2 * 224 * 224] = imageData[i * 4 + 2] / 255; // Blue channel
+      }
 
+      const inputTensor = new ort.Tensor('float32', inputArray, [1, 3, 224, 224]);
+      const feeds = { data: inputTensor };
 
-  const feeds = { data: inputTensor };
-  // Run inference
-  const output = await session.run(feeds);
+      // Run inference
+      model_mobilenet.run(feeds).then((output) => {
+        // Process the inference results
+        console.log('Inference Results:', output);
+        const outputData = output.mobilenetv20_output_flatten0_reshape0.data;
 
-  // Process the inference results
-  console.log('Inference Results:', output);
-  // alert(output);
+        // Find the index of the maximum value
+        let maxIndex = 0;
+        let maxValue = outputData[0];
 
-  const outputData = output.mobilenetv20_output_flatten0_reshape0.data; // Get the data from the output tensor
+        for (let i = 1; i < outputData.length; i++) {
+          if (outputData[i] > maxValue) {
+            maxValue = outputData[i];
+            maxIndex = i;
+          }
+        }
 
+        console.log('Index of the maximum value:', maxIndex);
 
-  // Find the index of the maximum value
-  let maxIndex = 0;
-  let maxValue = outputData[0];
-  console.log(outputData);
-  for (let i = 1; i < outputData.length; i++) {
-    if (outputData[i] > maxValue) {
-      maxValue = outputData[i];
-      maxIndex = i;
-    }
-  }
+        const labelsFileUrl = '../labels.txt';
+        get_image_label_mobilenet(labelsFileUrl, maxIndex);
+      });
+    };
 
-  console.log('Index of the maximum value:', maxIndex);
+    resolve();
+  });
 
-
-  const labelsFileUrl = '../labels.txt';
-  get_image_label_mobilenet(labelsFileUrl, maxIndex);
+  // model_mobilenet = null;
 
 };
 
@@ -178,92 +202,92 @@ function get_image_label_mobilenet(labelsFileUrl, maxIndex) {
 async function processImage_smarter(imageFile) {
   const image = new Image();
   image.src = URL.createObjectURL(imageFile);
-
-  if (model_encoder == null) {
-    model_encoder = await ort.InferenceSession.create('../model/vitgpt2/encoder_model_fp16_inputf32.onnx');
-  }
-  if (model_decoder == null) {
-    model_decoder = await ort.InferenceSession.create('../model/vitgpt2/decoder_model_fp16_inputf32.onnx');
-  }
-
-  console.log("model loaded")
-
-  const predictionBox = document.getElementById('predictionBox');
   const predictedLabelElement = document.getElementById('predictedLabel');
 
 
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-  canvas.width = 224;
-  canvas.height = 224;
-  ctx.drawImage(image, 0, 0, 224, 224);
-  const imageData = ctx.getImageData(0, 0, 224, 224).data;
-
-
-  console.log(image.height);
-  console.log(image.width);
-
-  const inputArray = new Float32Array(3 * 224 * 224);
-
-  for (let i = 0; i < 224 * 224; i++) {
-    inputArray[i] = (imageData[i * 4] * 0.00392156862745098 - 0.5) / 0.5; // Red channel
-    inputArray[i + 224 * 224] = (imageData[i * 4 + 1] * 0.00392156862745098 - 0.5) / 0.5; // Green channel
-    inputArray[i + 2 * 224 * 224] = (imageData[i * 4 + 2] * 0.00392156862745098 - 0.5) / 0.5; // Blue channel
+  if (model_encoder == null) {
+    model_encoder = await ort.InferenceSession.create('../model/vitgpt2/encoder_model_fp16_inputf32.onnx', { executionProviders: ['wasm'] });
+  }
+  if (model_decoder == null) {
+    model_decoder = await ort.InferenceSession.create('../model/vitgpt2/decoder_model_fp16_inputf32.onnx', { executionProviders: ['wasm'] });
   }
 
-  const inputTensor = new ort.Tensor("float32", inputArray, [1, 3, 224, 224]);
+  await new Promise((resolve) => {
+    image.onload = async () => {
+      console.log("model loaded");
 
-  console.log("input tensor : ", inputTensor);
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      canvas.width = 224;
+      canvas.height = 224;
+      ctx.drawImage(image, 0, 0, 224, 224);
+      const imageData = ctx.getImageData(0, 0, 224, 224).data;
 
-  const feeds = { "pixel_values": inputTensor };
-  // Run inference
-  const encoder_output = await model_encoder.run(feeds);
+      const inputArray = new Float32Array(3 * 224 * 224);
 
-  // const decoder = await ort.InferenceSession.create('../model/vitgpt2/decoder_model_fp16_inputf32.onnx');
+      for (let i = 0; i < 224 * 224; i++) {
+        inputArray[i] = (imageData[i * 4] * 0.00392156862745098 - 0.5) / 0.5; // Red channel
+        inputArray[i + 224 * 224] = (imageData[i * 4 + 1] * 0.00392156862745098 - 0.5) / 0.5; // Green channel
+        inputArray[i + 2 * 224 * 224] = (imageData[i * 4 + 2] * 0.00392156862745098 - 0.5) / 0.5; // Blue channel
+      }
 
+      const inputTensor = new ort.Tensor("float32", inputArray, [1, 3, 224, 224]);
+      const feeds = { "pixel_values": inputTensor };
 
-  let inputArrayId = new BigInt64Array(1 * 1);
-  inputArrayId[0] = BigInt(50256);
-  const inputTensorId = new ort.Tensor("int64", inputArrayId, [1, 1]);
+      // Run encoder inference
+      const encoder_output = await model_encoder.run(feeds);
 
+      let inputArrayId = new BigInt64Array(1 * 1);
+      inputArrayId[0] = BigInt(50256);
+      const inputTensorId = new ort.Tensor("int64", inputArrayId, [1, 1]);
 
-  const encoder_hidden_states = new ort.Tensor("float32", encoder_output.last_hidden_state.data, [1, 197, 768]);
-  decoder_output = await model_decoder.run({ "encoder_hidden_states": encoder_hidden_states, "input_ids": inputTensorId })
-  // Process the inference results
+      const encoder_hidden_states = new ort.Tensor("float32", encoder_output.last_hidden_state.data, [1, 197, 768]);
 
+      // Run decoder inference
+      let decoder_output = await generateText(model_decoder, encoder_hidden_states, inputTensorId);
 
-  let size_input = 1;
-  const decoder_output_data = decoder_output.logits.data;
+      let size_input = 1;
+      let maxIndex = findMaxValueAndIndex(decoder_output.logits.data).maxIndex;
+      let sentence = "";
 
+      while (maxIndex != 50256) {
+        const word = await getWordFromIndex(maxIndex);
+        sentence += word.replace("Ġ", "") + " ";
+        predictedLabelElement.textContent = `🤖: ${sentence}`;
 
+        // Update input for the next iteration
+        inputArrayId = appendToInputArray(inputArrayId, size_input, BigInt(maxIndex));
+        size_input += 1;
+        const inputTensorId = new ort.Tensor("int64", inputArrayId, [1, size_input]);
 
-  maxIndex = findMaxValueAndIndex(decoder_output_data).maxIndex;
-  let sentence = "";
-  while (maxIndex != 50256) {
-    const word = await getWordFromIndex(maxIndex);
-    console.log(word);
-    sentence += word.replace("Ġ", "") + " ";
-    predictedLabelElement.textContent = `🤖: ${sentence}`;
+        // Run decoder inference with updated input
+        decoder_output = await generateText(model_decoder, encoder_hidden_states, inputTensorId);
 
+        maxIndex = findMaxValueAndIndex(decoder_output.logits.data.slice(50257 * (size_input - 1), 50257 * size_input)).maxIndex;
+      }
 
-    // create a new inputArrayId which concatenate the old inputArrayId with the new maxIndex 
-    tmpInputArrayId = new BigInt64Array(size_input + 1);
-    tmpInputArrayId.set(inputArrayId);
-    tmpInputArrayId[size_input] = BigInt(maxIndex);
-    inputArrayId = tmpInputArrayId;
-    size_input += 1;
+      resolve(); // Resolve the promise after image processing
+    };
 
-    const inputTensorId = new ort.Tensor("int64", inputArrayId, [1, size_input]);
-    decoder_output = await model_decoder.run({ "encoder_hidden_states": encoder_hidden_states, "input_ids": inputTensorId })
+    // Ensure the image.onload is triggered
+    if (image.complete && image.naturalWidth !== 0) {
+      image.onload();
+    }
+  });
+}
 
-    const decoder_output_data = decoder_output.logits.data;
+// Helper functions
+async function generateText(model, encoder_hidden_states, inputTensorId) {
+  return await model.run({ "encoder_hidden_states": encoder_hidden_states, "input_ids": inputTensorId });
+}
 
-    maxIndex = findMaxValueAndIndex(decoder_output_data.slice(50257 * (size_input - 1), 50257 * size_input)).maxIndex;
+function appendToInputArray(inputArrayId, size_input, newMaxIndex) {
+  const tmpInputArrayId = new BigInt64Array(size_input + 1);
+  tmpInputArrayId.set(inputArrayId);
+  tmpInputArrayId[size_input] = newMaxIndex;
+  return tmpInputArrayId;
+}
 
-
-  }
-
-};
 
 // Find the max value and its index in an array
 function findMaxValueAndIndex(array) {
